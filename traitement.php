28bcfdea89cd826e658f8702766da43d187aca23
MamazonPremium <125975456+MamazonPremium@users.noreply.github.com>
2023-05-05 11:18:28 +0200

    Add files via upload
    connexion qui marche

--- traitement.php
+++ traitement.php
@@ -9,26 +9,48 @@
   }
 
   if (isset($_POST['valider'])){
-    echo "<H1>Méthode POST utilisée</H1>";
+    echo "je vien de inscription";
     $mail = $_POST['mail'];
     $password = $_POST['password'];
     echo "<ul>";
     echo '<li>' .'Mail : ' .$mail .'</li><BR>';
     echo '<li>' .'password : ' .$password .'</li><BR>';
     echo "</ul>";
-  }
+    $sqlQuery = 'INSERT INTO test_table(Email, Mdp) VALUES (:Email, :Mdp)';
+    echo "étape 1";
   
+    
+    $insertRecipe = $db->prepare($sqlQuery);
+    echo "étape 2";
+  
+    $insertRecipe->execute([
+      'Email' => $mail,
+      'Mdp' => $password,
+    ]);
+    echo "étape 3";
+  
+  }
 
-  $sqlQuery = 'INSERT INTO test_table( Email, Mdp) VALUES (:Email, :Mdp)';
-  echo "étape 1";
-
-
-  $insertRecipe = $db->prepare($sqlQuery);
-  echo "étape 2";
-
-  $insertRecipe->execute([
-    'Email' => $mail,
-    'Mdp' => $password,
-  ]);
-  echo "étape 3";
- ?>
+  if (isset($_POST['connexion'])) {
+    $mail = $_POST['mail'];
+    $password = $_POST['password'];
+    $sqlQuery = 'SELECT * FROM test_table WHERE Email = :Email AND Mdp = :Mdp';
+    
+    $result = $db->prepare($sqlQuery);
+    $result->execute([
+        'Email' => $mail,
+        'Mdp' => $password,
+    ]);
+    
+    $user = $result->fetch();
+    
+    if ($user) {
+        // L'utilisateur existe dans la base de données
+        header('Location: Accueil.html');
+        exit; // Arrête l'exécution du script après la redirection
+    } else {
+        // L'utilisateur n'existe pas dans la base de données
+        echo "Email ou mot de passe incorrect.";
+    }
+}
+?>
